898f599c9dc6b47ff941ee7b3da53d02262b8ec3
Ekdvs <ekdvsampath02@gmail.com>
2025-08-19 18:19:11 +0530

    chat schema create

---
+++ models/ChatModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const chatSchema=new mongoose.Schema(
+    {
+        chatName:{
+            type:String,
+            trim:true,
+        },
+        isGroupChat:{
+            type:Boolean,
+            default:false,
+        },
+        users:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'User'
+            }
+        ],
+        latestMessage:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
+        groupAdmin:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'User'
+            }
+        ],
+
+    },
+    {
+        timestamps:true,
+    }
+)
+const ChatModel =mongoose.model('Chat',chatSchema);
+export default ChatModel;
